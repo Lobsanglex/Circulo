22f7014a733e251dd5e97ad5b5e97317e1bb8c80
Robinson Gonzalez <rgonzalez@ifk.cl>
2021-07-25 01:05:28 -0400

    ultimo 25-07-2021

--- js/inicio.js
+++ js/inicio.js
@@ -26,9 +26,71 @@
         }
 
         function miembro(n){
-            // window.open("miembro1.html","_self");
-            var eti = "<p>Miembro "+n+"</p>";
-            eti += "<a href='miembros/miembro"+n+".html'>Ver más</a>";
+          var foto = '';
+          var fundador = "";
+          var apodo = '';
+          var nombre = '';
+          var cargo = '';
+          var ruta = '';
+          switch(n) {
+            case 1:
+              foto = '../img/ft1.png';
+              fundador ="<img src='../img/logo_fundador.svg' class='logo_fundador'>";
+              nombre = 'Pablo Fuente';
+              cargo = 'Presidente';
+              ruta = 'miembros/juan_pablo.html';
+              break;
+            case 2:
+              foto = '../img/ft2.png';
+              fundador ="<img src='../img/logo_fundador.svg' class='logo_fundador'>";
+              nombre = 'Raul Salas';
+              cargo = 'Vice Presidente';
+              ruta = 'miembros/raul_salas.html';
+              break;
+            case 3:
+              foto = '../img/ft3.png';
+              fundador ="<img src='../img/logo_fundador.svg' class='logo_fundador'>";
+              nombre = 'Juan Jose';
+              cargo = 'Miembro';
+              ruta = 'miembros/juan_jose.html';
+              break;
+            case 4:
+              foto = '../img/ft4.png';
+              nombre = 'Alex Vejar';
+              cargo = 'Sargento de Armas';
+              ruta = 'miembros/alex_vejar.html';
+              break;
+            case 5:
+              foto = '../img/fot_t1.png';
+              nombre = 'Nicollette Grace';
+              cargo = 'Secretaria';
+              ruta = 'miembros/nicollette_grace.html';
+              break;
+            default:
+              break;
+          }
+          var eti = "";
+            eti += "<div class='card'>";
+            eti += "  <div class='circle_t'>";
+            eti += "    <div class='img_t'>";
+            eti += "      <img src='"+foto+"'>";
+            eti += "    </div>";
+            eti += "  </div>";
+            eti += "  <div class='content_t'>";
+            eti += "    <a href='#'>";
+            eti += "      <i class='fa fa-whatsapp' aria-hidden='true'></i>";
+            eti += "    </a>";
+            eti += fundador;
+            eti += "    <h3>"+nombre+"</h3>";
+            eti += "    <div class='textIcon_t'>";
+            eti += "      <h4>"+cargo+"</h4>";
+            eti += "      <a href='"+ruta+"'>";
+            eti += "        <i class='fa fa-arrow-right' aria-hidden='true'></i>";
+            eti += "      </a>";
+            eti += "    </div>";
+            eti += "  </div>";
+            eti += "</div>";
+            // eti += "<a href='miembros/miembro"+n+".html'>Ver más</a>";
             document.getElementById('casilla_miembros').innerHTML = eti;
         }
 
